3d380e21647829f002f3f7953cb3a44990d9a6c5
Edgar Cortez <edgar10019@gmail.com>
2015-11-21 18:29:59 -0600

    put all tabs in window into array

---
+++ src/popup.js
@@ -0,0 +1,19 @@
+
+
+function getCurrentTabUrl(callback) {
+  var queryInfo = {			//condition that makes all tabs in current window to be put into array
+    currentWindow: true
+  };
+
+  chrome.tabs.query(queryInfo, function(tabs) { //this calls the queryInfo in which puts into array
+
+  var holdTabs = []; //will hold our url's
+  var index;
+  	for( index = 0; index < tabs.length; index ++ ){
+  		holdTabs[index] = tabs[index].url; // each tab url is in array holdTabs
+  	}
+
+
+});
+
+}
